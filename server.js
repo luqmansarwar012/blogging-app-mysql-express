ffa4b360fdc2c0c84514b2e7d8e146ee76e14660
luqman sarwar <luqmanrajput012@gmail.com>
2024-01-25 10:37:00 +0500

    chore(git): update .gitignore
    This commit updates the .gitignore file to exclude the following folders:
    
    - .env : ignore environment variables file

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`App running at port: ${port}`);
+  console.log(`App running on port: ${port}`);
 });
