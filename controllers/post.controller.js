2c407927d58337b54c077b58b574e79cc331eb23
luqman sarwar <luqmanrajput012@gmail.com>
2024-01-25 16:26:05 +0500

    feat: add initial folder structure for controllers and routes
    This commit introduces the initial folder structure for handling routes and controllers in the project. Key changes include:
    
    - **controllers/**: Directory to house application controllers.
    - **routes/**: Directory to organize route handling.

---
+++ controllers/post.controller.js
@@ -0,0 +1,6 @@
+const index = (req, res) => {
+  console.log("index");
+  res.send("index");
+};
+
+module.exports = { index };
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const postsRoute = require("./routes/posts");
 require("dotenv").config();
 const app = express();
 
+// App routes
+app.use("/posts", postsRoute);
+
+// Listening to port
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`App running on port: ${port}`);
 });
+
+module.exports = app;
