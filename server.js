20d9ce1f76587e72deb27afb1033820a32f51dc6
luqman sarwar <luqmanrajput012@gmail.com>
2024-01-25 09:55:12 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`App running at port: ${port}`);
+});
